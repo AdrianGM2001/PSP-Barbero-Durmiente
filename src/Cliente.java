e537f0cace69b7396bc164edd38a7ea59eebe30c
Adrian Gonzalez <adriangonzalezmartos@proton.me>
2024-10-30 11:46:03 +0100

    Clase Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,19 @@
-public class Cliente {
+public class Cliente implements Runnable {
+    private String nombre;
+    private Barberia barberia;;
 
-    
+    public Cliente(String nombre, Barberia barberia) {
+        this.nombre = nombre;
+        this.barberia = barberia;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            barberia.sentar(nombre);
+            wait();
+            barberia.cortar();
+            Thread.sleep(3000);
+        }
+    }
 }

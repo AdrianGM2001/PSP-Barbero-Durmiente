edb4e73b41ac56562e9d2816355baae0201d0abf
Gonzalo Hernández García <gonzalohgmaster@gmail.com>
2024-10-30 11:34:06 +0100

    barberia

--- src/Barberia.java
+++ src/Barberia.java
@@ -1,3 +1,9 @@
 public class Barberia {
-    
+    private enum ESTADO{DORMIDO,OCUPADO}
+    private ESTADO estado;
+    private int numeroSillas;
+   
+    public Barberia(int numeroSillas) {
+        this.numeroSillas = numeroSillas;
+    } 
 }
